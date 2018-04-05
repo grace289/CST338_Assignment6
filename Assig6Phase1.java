bc4fe3ca4154fed66ee7b696c11753fb41facab1
grace289 <33207084+grace289@users.noreply.github.com>
2018-04-04 19:21:22 -0700

    Update and rename Assignment6.java to Assig6Phase1.java

---
+++ Assig6Phase1.java
@@ -0,0 +1,7 @@
+/*
+ * Assignment M6 Phase 1
+ * Model-View-Controller Design Pattern
+ * Authors: Christian Guerrero, Jose Garcia, Grace Alvarez, Gabriel Loring
+ * Last Changed: April 4th, 2018
+ *
+ */
